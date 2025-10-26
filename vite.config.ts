0e39c7702a25ecc5fe63ce19fb6d93486c191cac
Michal Maj <michalmajdev@gmail.com>
2025-10-26 23:29:30 +0100

    GitHub Pages fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/majunez/" : "/",
   plugins: [react()],
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".mjs", ".mts", ".json"],
   },
-});
+}));
